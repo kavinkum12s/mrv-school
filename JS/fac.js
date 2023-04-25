ef8015fa11258b796f0326fe3271a961c3ae4886
Kavin <kavinvinoth3@gmail.com>
2023-04-25 21:37:59 +0530

    fac

---
+++ JS/fac.js
@@ -0,0 +1,41 @@
+function Infrastructure() {
+    document.getElementById("infra").style.display="block";
+    document.getElementById("transport").style.display="none";
+    document.getElementById("lab").style.display="none";
+    document.getElementById("hostel").style.display="none";
+    document.getElementById("libaray").style.display="none";
+   
+}
+
+function transport() {
+    document.getElementById("transport").style.display="block";
+    document.getElementById("infra").style.display="none";
+    document.getElementById("lab").style.display="none";
+    document.getElementById("libaray").style.display="none";
+    document.getElementById("libaray").style.display="none";
+
+}
+function lab() {
+    document.getElementById("transport").style.display="none";
+    document.getElementById("infra").style.display="none";
+    document.getElementById("lab").style.display="block";
+    document.getElementById("hostel").style.display="none";
+    document.getElementById("libaray").style.display="none";
+}
+function hostel() {
+    document.getElementById("transport").style.display="none";
+  
+    document.getElementById("infra").style.display="none";
+    document.getElementById("lab").style.display="none";
+    document.getElementById("hostel").style.display="block";
+    document.getElementById("libaray").style.display="none";
+}
+function libaray() {
+    document.getElementById("transport").style.display="none";
+    document.getElementById("infra").style.display="none";
+    document.getElementById("lab").style.display="none";
+    document.getElementById("hostel").style.display="none";
+    document.getElementById("libaray").style.display="block";
+}
+
+
